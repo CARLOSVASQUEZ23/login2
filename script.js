afc63100082196c2bfc8737106c5ed9630891807
Carlos Vasquez <carloscfpvasquez@gmail.com>
2024-10-04 08:54:35 -0600

    Se creo un nuevo repositorio

---
+++ script.js
@@ -0,0 +1,18 @@
+let usuarioCorrecto = "admin";
+let contraseñaCorrecta = "admin2024";
+
+function login(){
+    //Capturar datos del DOM
+    let usuario = document.getElementById("usuario").value;
+    let contraseña = document.getElementById("contraseña").value;
+    let resultado = document.getElementById("resultado");
+
+    if (usuario == usuarioCorrecto && contraseña == contraseñaCorrecta) {
+        resultado.style.color = "green";
+        resultado.innerHTML = "Acceso correcto";
+    }else{
+        resultado.style.color = "red";
+        resultado.innerHTML = "Acceso incorrecto"
+    }
+
+}
